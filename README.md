52524086a2c044bc9bc7aa3856e7119d44e71580
Shaurya Choudhary <shaurya.src@gmail.com>
2020-11-15 03:36:46 +0530

    Created main.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Word Cloud Generator
+
+#### Sample Dataset: https://www.kaggle.com/zynicide/wine-reviews/data
---
+++ main.py
@@ -0,0 +1,8 @@
+from os import path
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from wordcloud import wordCloud, STOPWORDS, ImageColorGenerator
+from PIL import Image
+
