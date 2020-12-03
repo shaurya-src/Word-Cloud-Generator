f50ed084ba9a2dfb4cf21faaac42fc82e3ab4054
Shaurya Choudhary <shaurya.src@gmail.com>
2020-12-03 21:58:28 +0530

    Minor changes

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from PIL import Image
 
-df = pd.read_csv("winemag-data-130k-v2.csv", index_col=0)
 
 text = " ".join(review for review in df.description)
 print ("There are {} words in the combination of all review.".format(len(text)))
